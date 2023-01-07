cf5c5a284ac592cf776c283a580e48c70adac5ad
cochaviz <cochavi.zohar@protonmail.com>
2023-01-07 12:07:11 +0100

    Improve startup ~experience~ (aka top bar finally hides itself properly)

--- config/polybar/launch_hidden.sh
+++ config/polybar/launch_hidden.sh
@@ -1,22 +1,17 @@
 #!/bin/bash
 
-$HOME/.config/polybar/launch.sh $1
-
-sleep 1
-
-# Wait for polybar to be launched
-until [[ $(ps aux | grep polybar) ]]; do
-	sleep 1
-done
-
 # Kill al instances of hideIt
 for PID in $(ps aux | grep hideIt.sh | grep polybar | grep -oP '^zohar\s+\K(\w+)\s'); do
-	kill -9 $PID
+	kill -9 "$PID"
 done
 
-# Run hideIt
-exec hideIt.sh --class '^polybar$' --direction top -p -$2 -H -S &> /dev/null &
+
+"$HOME"/.config/polybar/launch.sh "$1"
+
+sleep 3
 
 # Remove top padding
 bspc config top_padding 0
 
+# Run hideIt
+exec hideIt.sh --class '^polybar$' --direction top -p -"$2" -H -S &> /dev/null
